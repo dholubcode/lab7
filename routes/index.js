40e604e227733f8bfd6b23584008aa935c099119
Dylan Holub <dholub@ucsd.edu>
2021-02-18 10:57:49 -0800

    lab7 stuff from today

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,14 @@ var projects = require('../projects.json');
 exports.view = function(request, response){
   	response.render('index', projects);
 };
+
+
+
+exports.view = function(req, res) {
+	data['viewAlt'] = false;
+	 res.render('index', data);
+}
+exports.viewAlt = function(req, res) {
+	data['viewAlt'] = true;
+	 res.render('index', data);
+}
